3edeabc321db5b022bac41c17731453f52ff1dc4
yordihaas <Pomuch1@DESKTOP-74VDPG6>
2026-03-03 07:39:11 -0600

    feat: estructura inicial del proyecto.

---
+++ src/main/java/com/mycompany/practicagit_sfdjdh/PracticaGit_SFDJDH.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicagit_sfdjdh;
+
+/**
+ *
+ * @author Pomuch1
+ */
+public class PracticaGit_SFDJDH {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        
+        
+        
+        
+        
+        
+     
+    }
+}
