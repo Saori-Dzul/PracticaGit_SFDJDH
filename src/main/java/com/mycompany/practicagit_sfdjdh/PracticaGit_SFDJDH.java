c123933a0fe191fce3989555a39175a34687ba60
yordihaas <Pomuch1@DESKTOP-74VDPG6>
2026-03-09 08:24:10 -0600

    fix: añadida calculadora básica.

--- src/main/java/com/mycompany/practicagit_sfdjdh/PracticaGit_SFDJDH.java
+++ src/main/java/com/mycompany/practicagit_sfdjdh/PracticaGit_SFDJDH.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.practicagit_sfdjdh;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Pomuch1
@@ -11,10 +13,19 @@ package com.mycompany.practicagit_sfdjdh;
 public class PracticaGit_SFDJDH {
 
     public static void main(String[] args) {
+        
         System.out.println("Hello World!");
         
+        System.out.println ("Bienvenido, usuario estudiante.");
         
-        
+    Scanner cin = new Scanner (System.in);
+    int n1, n2, suma;    
+    System.out.print("Introduce primer numero: ");
+    n1 = cin.nextInt();
+    System.out.print("Introduce segundo numero: ");
+    n2 = cin.nextInt();
+    suma = n1 + n2;    
+    System.out.println("El resultado es: " + suma);
         
         
         
