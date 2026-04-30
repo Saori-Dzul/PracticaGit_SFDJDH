7067ae9a0dfaba66b08c81f79599e8aec766c044
Saori-Dzul <saori.dzul_pm25234@cecyte.edu.mx>
2026-04-30 11:51:17 -0600

    Add files via upload

---
+++ Area_rec.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Pomuch1
+ */
+public class Area_rec {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Scanner Scanner = new Scanner (System.in);
+   
+    for (int n = 1; n <= 10; n++ ){
+        
+    
+    System.out.print ("teclee la base: ");
+    int base = Scanner.nextInt();
+  
+    System.out.print ("teclee la base: ");
+    int altura = Scanner .nextInt();
+   
+    int area_rec=base * altura ;
+  
+    System.out.println("El area del rectangulo es :"+ area_rec);   
+        
+    }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
